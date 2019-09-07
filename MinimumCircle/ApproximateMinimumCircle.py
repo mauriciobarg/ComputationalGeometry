3e0e7078abec039da14362b3bc8d0330fa961cca
Mauricio Barg <mrwbarg@gmail.com>
2019-09-07 13:10:58 -0300

    Add folder

---
+++ MinimumCircle/ApproximateMinimumCircle.py
@@ -0,0 +1,38 @@
+from typing import Type, List
+from GeometricEntities import Point, Circle
+
+
+def ApproximateMinimumCircle(points_list: List[Type[Point]]) -> Type[Circle]:
+
+    (min_x, max_x) = min(points_list, key=lambda x: x.getX()), max(
+        points_list, key=lambda x: x.getX())
+
+    (min_y, max_y) = min(points_list, key=lambda x: x.getY()), max(
+        points_list, key=lambda x: x.getY())
+
+    possible_pairs = [(min_x, max_x), (min_y, max_y)]
+
+    max_distance = 0
+    starting_pair = None
+
+    for pair in possible_pairs:
+        distance = pair[0].distanceToPointSquared(pair[1])
+        if (distance > max_distance):
+            starting_pair = pair
+            max_distance = distance
+
+    points_list.remove(starting_pair[0])
+    points_list.remove(starting_pair[1])
+
+    minimum_circle = Circle(starting_pair[0].middlePointToPoint(starting_pair[1]),
+                            starting_pair[0].distanceToPoint(starting_pair[1]) / 2)
+
+    for point in points_list:
+        if not (minimum_circle.isPointInside(point)):
+            c = minimum_circle.getCenter()
+            d = point.distanceToPoint(c)
+            minimum_circle.moveCenter(
+                (d - minimum_circle.getRadius()) / 2, point.getVectorToPoint(c))
+            minimum_circle.varyRadius((d - minimum_circle.getRadius()) / 2)
+
+    return minimum_circle
---
+++ MinimumCircle/main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from GeometricEntities import Point
+from ApproximateMinimumCircle import ApproximateMinimumCircle
+from MinimumCircle import MinimumCircle
+from random import randrange
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy
+import datetime
+import os
+
+print(os.getcwd())
+data = pd.read_csv('MinimumCircle/points.txt', sep="   ", header=None, engine='python', names=['x', 'y'])
+
+points = []
+for i in range(data.shape[0]):
+    points.append(Point(data.iloc[i][0], data.iloc[i][1]))
+
+approximate_minimum_circle = ApproximateMinimumCircle(points[:])
+minimum_circle = MinimumCircle(points[:])
+
+print("Círculo Aproximado: ", approximate_minimum_circle.getCenter().getCoordinates(), approximate_minimum_circle.getRadius())
+print("Círculo: ", minimum_circle.getCenter().getCoordinates(), minimum_circle.getRadius())
+
+approximate_min_circle_plot = plt.Circle(approximate_minimum_circle.getCenter().getCoordinates(), radius=approximate_minimum_circle.getRadius(), fill=False, color='r')
+min_circle_plot = plt.Circle(minimum_circle.getCenter().getCoordinates(), radius=minimum_circle.getRadius(), fill=False)
+
+plt.scatter(data['x'], data['y'])
+fig = plt.gcf()
+ax = fig.gca()
+ax.add_artist(approximate_min_circle_plot)
+ax.add_artist(min_circle_plot)
+plt.xlim((100, 700))
+plt.ylim((100, 500))
+ax.set_aspect('equal')
+plt.show()
+
+def CompareRunTimes():
+
+    n = []
+    run_time_exact = []
+    run_time_approximate = []
+
+    for i in range(99, 2001):
+        n.append(i)
+
+        random_points = []
+        for j in range(i):
+            random_points.append(Point(randrange(0, 1000), randrange(0, 1000)))
+        
+        start_time = datetime.datetime.now()
+        ApproximateMinimumCircle(random_points[:])
+        finish_time = datetime.datetime.now()
+
+        run_time_approximate.append((finish_time - start_time).total_seconds())
+
+        start_time = datetime.datetime.now()
+        ApproximateMinimumCircle(random_points[:])
+        finish_time = datetime.datetime.now()
+
+        run_time_exact.append((finish_time - start_time).total_seconds())
+
+        print("Progress: ", i)
+
+        z1 = numpy.polynomial.polynomial.polyfit(n, run_time_approximate, 1)
+        p1 = numpy.poly1d(z1)
+
+        z2 = numpy.polynomial.polynomial.polyfit(n, run_time_exact, 1)
+        p2 = numpy.poly1d(z2)
+
+    plt.plot(n, p1(n), "r--")
+    plt.plot(n, p2(n), "--")
+    plt.show()
+
+# CompareRunTimes()
+
+  
+
